1e4d317e52f79dfb5ba1ce3effe047e2b9ddbb18
Adrian Rivera <riveracavana@gmail.com>
2020-03-19 14:50:47 -0600

    post to for sale

--- db/cars/sale_car.sql
+++ db/cars/sale_car.sql
@@ -1,5 +1,5 @@
 insert into car_sale(
-    car_id, user_id, description, milage, price, location, sold
+    car_id, user_id,description, milage, price, location, sold
 ) values (
     $1, $2, $3, $4, $5, $6, false
 )
