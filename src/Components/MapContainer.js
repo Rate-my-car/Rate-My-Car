e32ea9584b4cdef837af3a6e3fe8f94b6185ed64
Tia Gollaher <tiaag2013@gmail.com>
2020-03-18 18:07:36 -0600

    map styling finished

--- src/Components/MapContainer.js
+++ src/Components/MapContainer.js
@@ -5,7 +5,7 @@ import './styling/Maps.scss'
 
 
 const MapContainer = (props) => { 
-    const [style, setStyle] = useState({width: '95%' , height: '60vh', overflow: "hidden"})
+    const [style, setStyle] = useState({width: '50vw' , height: '100vh'})
     const [zipCode, setZipCode] = useState(0)
     const [showInfoWindow, toggleInfoWindow] = useState(false)
     const[activeMarker, setActiveMarker] = useState({})
@@ -60,59 +60,67 @@ useEffect(() => {
 
     return( 
         
-        <div className='map-container'> 
-            <h4 className='search-area-text'>Search in area:</h4>
-            <input className='zipcode-input' placeholder = 'Enter Zip Code' onChange = { e => setZipCode(e.target.value)} />
-            <button className='map-search-btn' onClick = {() => searchLocations(zipCode)}>Search</button>
-            <div className='google-map'>
-                <Map 
-                    google={props.google}
-                    onClick={onMapClicked}
-                    center = {center}
-                    style = {style}
-                    zoom = {12}>
-                
-                {locations? locations.map((element,index) => 
-                
-                    <Marker 
-                        onClick={onMarkerClick}
-                        position = {element.geometry.location}
-                        label = {(index + 1).toString()}
-                    />): console.log('no locations')}
-
-                <InfoWindow
-                    marker={activeMarker}
-                    visible={showInfoWindow}>
-                    <div>
-                        <h1>{markerInfo.name}</h1>
-                        <p>Address:</p>
-                        <p>{markerInfo.formatted_address}</p>
-                        <p>Rating:</p>
-                        <p>{markerInfo.rating}</p>
-                        <a 
-                            href = {`https://www.google.com/maps/place/${markerInfo.formatted_address}`} 
-                            rel="noopener noreferrer"
-                            target="_blank">
-                            get directions</a>
-                    </div>
-                </InfoWindow>
-
-                </Map>
+        <div className='map-container'>
+            <div className='map-container-header'>
+                <h1>Service & Parts Locator</h1> 
+                <div className='map-container-search'>
+                    <h4 className='search-area-text'>Search in area:</h4>
+                    <input className='zipcode-input' placeholder = 'Enter Zip Code' onChange = { e => setZipCode(e.target.value)} />
+                    <button className='map-search-btn' onClick = {() => searchLocations(zipCode)}>Search</button>
+                </div>
             </div>
-            
+        <div className='map-business-listing-container'>
+            <div className='google-map-container'>
+        <Map 
+            google={props.google}
+            onClick={onMapClicked}
+            center = {center}
+            // style = {style}
+            zoom = {12}
+        >
+        
         {locations? locations.map((element,index) => 
-        <div> 
-        <p>Marker: {index + 1} </p>
-        <h3>{element.name}</h3>
-        <p>Address:</p>
-        <p>{element.formatted_address}</p>
-        <p>Rating:</p>
-        <p>{element.rating}</p>
+        
+            <Marker 
+                onClick={onMarkerClick}
+                position = {element.geometry.location}
+                label = {(index + 1).toString()}
+            />): console.log('no locations')}
 
+        <InfoWindow
+            marker={activeMarker}
+            visible={showInfoWindow}>
+            <div>
+                <h3>{markerInfo.name}</h3>
+                <p className='marker-text'>{markerInfo.formatted_address}</p>
+                <p className='marker-text'>Rating: {markerInfo.rating} of 5</p>
+                <a 
+                    href = {`https://www.google.com/maps/place/${markerInfo.formatted_address}`} 
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className='marker-directions-text'>
+                    get directions</a>
+            </div>
+        </InfoWindow>
+
+        </Map>
         </div>
-       ): console.log('no locations')}
-
-
+        <div className='business-listings'> 
+        {locations? locations.map((element,index) => 
+        <div className='business-listing'> 
+            <h4 className='business-listing-marker'>Marker: {index + 1} </h4>
+            <h3>{element.name}</h3>
+            <hr/>
+            <p className='business-listing-info'>{element.formatted_address}</p>
+            <p className='business-listing-info'>Rating: {element.rating} of 5</p>
+        </div>
+       ) : (
+       <div className='business-listings-bkgd'>Search to view Service & Parts Locations</div>)
+        // console.log('no locations')
+        }
+       
+    </div>
+        </div>
         </div>
     )
 }
